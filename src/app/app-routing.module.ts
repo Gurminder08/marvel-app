bd5b8182f59e2b489afa4ad26552b2add00f9c58
gurminder <gurminder95@gmail.com>
2019-09-09 12:43:49 -0300

    adding updates to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: './home/home.module#HomePageModule'},
+  { path: 'characters', loadChildren: './pages/characters/characters.module#CharactersPageModule' }
 ];
 
 @NgModule({
