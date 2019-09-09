e2d70cf3fde69501047ffd8004b00f01578e1fd9
gurminder <gurminder95@gmail.com>
2019-09-09 15:38:39 -0300

    adding character-comics page

---
+++ src/app/pages/character-comics/character-comics.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-comics',
+  templateUrl: './character-comics.page.html',
+  styleUrls: ['./character-comics.page.scss'],
+})
+export class CharacterComicsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
