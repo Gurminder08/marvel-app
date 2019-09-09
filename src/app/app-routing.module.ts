019aaeca59489a004a7c72127ad91d011143cd45
gurminder <gurminder95@gmail.com>
2019-09-09 15:40:21 -0300

    modified app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule'},
-  { path: 'characters', loadChildren: './pages/characters/characters.module#CharactersPageModule' }
+  { path: 'characters', loadChildren: './pages/characters/characters.module#CharactersPageModule' },
+  { path: 'characters/:id/comics', loadChildren: './pages/character-comics/character-comics.module#CharacterComicsPageModule' }
 ];
 
 @NgModule({
