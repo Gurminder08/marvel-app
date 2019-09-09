9d23a427c77d46bd19d42cb9b55ca8999a3b49a7
gurminder <gurminder95@gmail.com>
2019-09-09 11:32:14 -0300

    adding characters API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
   production: true
 };
+
+
+export const BASE_URL = 'http://gateway.marvel.com/v1/public/';
+export const TS = '1';
+export const API_KEY = '136ce14c4f761a776680f1a415560c94';
+export const MD5_HASH = 'ebf87bbcc3d27ec4a5e57730c45cf19b';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,11 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+//http://gateway.marvel.com/v1/public/characters?ts=1&apikey=136ce14c4f761a776680f1a415560c94&hash=ebf87bbcc3d27ec4a5e57730c45cf19b
+export const BASE_URL = 'http://gateway.marvel.com/v1/public';
+export const TS = '1';
+export const API_KEY = '136ce14c4f761a776680f1a415560c94';
+export const MD5_HASH = 'ebf87bbcc3d27ec4a5e57730c45cf19b';
+
+//http://gateway.marvel.com/v1/public/characters?ts=1&apikey=136ce14c4f761a776680f1a415560c94&hash=ebf87bbcc3d27ec4a5e57730c45cf19b
+
