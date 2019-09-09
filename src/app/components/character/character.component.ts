deab537064b54c9dc4cf78f9370aebe5ed018fda
gurminder <gurminder95@gmail.com>
2019-09-09 12:49:33 -0300

    adding character component and navigate to character-detail comp.

---
+++ src/app/components/character/character.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { Character } from '../../models/character';
+
+@Component({
+  selector: 'app-character',
+  templateUrl: './character.component.html',
+  styleUrls: ['./character.component.scss'],
+})
+export class CharacterComponent implements OnInit {
+
+  @Input() character: Character = <Character>{};
+  @Input() query: string;
+
+  imageSource: any;
+
+  constructor(private router: Router) {
+    
+   }
+
+  ngOnInit() {
+    this.imageSource = `${this.character.thumbnail.path}.${this.character.thumbnail.extension}`;
+  }
+
+  openCharacterDetailsWithParams(){
+    let navigationExtras: NavigationExtras = {
+      state: {
+        character: this.character
+      }
+    };
+    this.router.navigate(['/characters',this.character.id], navigationExtras);
+  }  
+}
+
