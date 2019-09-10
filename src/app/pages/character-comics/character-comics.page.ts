931ad90634689b9b02ac7408ff319408ca1f9415
gurminder <gurminder95@gmail.com>
2019-09-10 16:20:31 -0300

    making service call to get character comics data

--- src/app/pages/character-comics/character-comics.page.ts
+++ src/app/pages/character-comics/character-comics.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CharacterComicsService } from 'src/app/services/character-comics.service';
+import { CharacterComics } from 'src/app/models/character-comics';
+
 
 @Component({
   selector: 'app-character-comics',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterComicsPage implements OnInit {
 
-  constructor() { }
+  id:number;
+  characterComics: CharacterComics[] = [];
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router, public api: CharacterComicsService) { 
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.id = this.router.getCurrentNavigation().extras.state.id;
+        console.log(this.id);
+      }
+    });
+  }
+
+    ngOnInit() {
+      this.getCharacterComics();
+  }
+
+  async getCharacterComics() {
+    await this.api.getCharacterComics(0, 20, this.id)
+      .subscribe(res => {
+        this.characterComics = res['data'].results;
+        console.log(this.characterComics[0].prices[0].price);
+      }, err => {
+        console.log(err);;
+      });
   }
 
 }
