dd71841c8d1aaec1fb18a76ee228395bae5f4a64
gurminder <gurminder95@gmail.com>
2019-09-11 02:39:24 -0300

    adding CharacterComicComponent to the declarations

--- src/app/pages/character-comics/character-comics.module.ts
+++ src/app/pages/character-comics/character-comics.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { CharacterComicsPage } from './character-comics.page';
 import { CharacterComicComponent } from 'src/app/components/character-comic/character-comic.component';
+import { CharacterComicDetailComponent } from 'src/app/components/character-comic-detail/character-comic-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CharacterComicsPage
+  },
+  {
+    path: ':id',
+    component: CharacterComicDetailComponent
   }
 ];
 
@@ -22,6 +25,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [CharacterComicsPage, CharacterComicComponent]
+  declarations: [CharacterComicsPage, CharacterComicComponent, CharacterComicDetailComponent]
 })
-export class CharacterComicsPageModule {}
+export class CharacterComicsPageModule { }
