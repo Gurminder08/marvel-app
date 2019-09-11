28f4da7aa94bbc8f662ceeabedac5f24dbbc672f
gurminder <gurminder95@gmail.com>
2019-09-11 19:54:32 -0300

    moving home page in pages folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule'},
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule'},
   { path: 'characters', loadChildren: './pages/characters/characters.module#CharactersPageModule' },
   { path: 'characters/:id/comics', loadChildren: './pages/character-comics/character-comics.module#CharacterComicsPageModule' }
 ];
