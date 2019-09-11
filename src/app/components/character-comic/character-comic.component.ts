c8583be8615c22a86fd39f311e7e4fbaf51558b8
gurminder <gurminder95@gmail.com>
2019-09-11 02:36:48 -0300

    routing to character comic detail comp

--- src/app/components/character-comic/character-comic.component.ts
+++ src/app/components/character-comic/character-comic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 import { CharacterComics } from 'src/app/models/character-comics';
 
 @Component({
@@ -11,10 +12,19 @@ export class CharacterComicComponent implements OnInit {
   @Input() characterComic: CharacterComics = <CharacterComics>{};
   imageSource: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.imageSource = `${this.characterComic.thumbnail.path}.${this.characterComic.thumbnail.extension}`;
   }
 
+  openCharacterComicsDetails(){
+    console.log('open character comics');
+    let navigationExtras: NavigationExtras = {
+      state: {
+        characterComic: this.characterComic
+      }
+    };
+    this.router.navigate([`${this.router.url}/${this.characterComic.id}`],navigationExtras); 
+  }
 }
